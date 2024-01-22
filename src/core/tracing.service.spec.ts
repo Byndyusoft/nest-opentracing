34e7b38ce536fb92da4400aabbdcf9a5073a28ba
KillWolfVlad <KillWolfVlad@users.noreply.github.com>
2024-01-22 15:57:46 +0500

    test: add test to normalize base url in span name

--- src/core/tracing.service.spec.ts
+++ src/core/tracing.service.spec.ts
@@ -1,10 +1,13 @@
 import { Span, MockTracer } from "opentracing";
+import { MockSpan, MockContext } from "opentracing/lib/mock_tracer";
 
 import { AsyncContext } from "../async-context";
 import { TracingService } from "./tracing.service";
 
 const asyncContext = AsyncContext.getInstance();
 const tracer = new MockTracer();
+
+jest.spyOn(tracer, "extract").mockImplementation(() => new MockContext(new MockSpan(tracer)));
 
 beforeEach(async () => {
   tracer.clear();
@@ -38,6 +41,35 @@ it("creates child span of rootSpan", async () => {
   const rootSpan = tracingService.getRootSpan();
 
   expect(childSpan.context().toTraceId()).toEqual(rootSpan.context().toTraceId());
+});
+
+describe("get span from request", () => {
+  const tracingService = new TracingService(tracer, asyncContext, { logBodies: true });
+
+  const defaultRequest = {
+    headers: {
+      "uber-trace-id": "someTraceId",
+    },
+    baseUrl: "/api/v1/locations/1/users/2",
+    method: "POST",
+    query: {
+      userVersion: 3,
+    },
+    body: {
+      name: "newName",
+    },
+  };
+
+  const rootSpan = tracingService.getSpanFromRequest(defaultRequest as never) as MockSpan;
+
+  it("normalize base url in span name", () => {
+    expect(rootSpan.operationName()).toBe("/api/v1/locations/#val/users/#val");
+  });
+
+  it("add params to tags", () => {
+    expect(rootSpan.tags()["http.params_1"]).toBe("1");
+    expect(rootSpan.tags()["http.params_2"]).toBe("2");
+  });
 });
 
 describe("trace async function", () => {
